e91e6106961128d2f8af0f21ebc0e2ec9c75396d
Sharvani Jadhav <sharvaniuj21@iitk.ac.in>
2024-06-12 10:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (95.13%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero=0,one=0,two=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) zero++;
+            if(nums[i]==1) one++;
+            if(nums[i]==2) two++;
+        }
+        for(int i=0;i<zero;i++){
+            nums[i]=0;
+        }
+        for(int i=zero;i<zero+one;i++){
+            nums[i]=1;
+        }
+        for(int i=zero+one;i<n;i++){
+            nums[i]=2;
+        }
+    }
+};
