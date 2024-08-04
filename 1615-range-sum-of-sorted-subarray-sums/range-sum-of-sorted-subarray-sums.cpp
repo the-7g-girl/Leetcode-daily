5872528a134e64e0cc1265c59b90e8a0eb2546a8
Sharvani Jadhav <sharvaniuj21@iitk.ac.in>
2024-08-04 18:03:55 +0530

    Time: 96 ms (38.20%) | Memory: 42.8 MB (6.52%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<long> sums;
+        for(int i=1; i<n; i++) {
+            nums[i]+= nums[i-1]%MOD;
+            nums[i] = nums[i]%MOD;
+        }
+        nums.insert(nums.begin(), 0);
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n+1; j++) {
+                sums.push_back((nums[j]-nums[i])%MOD);
+            }
+        }
+
+        sort(sums.begin(), sums.end());
+
+        int ans = 0;
+
+        for(int i= left-1; i<right; i++) {
+            if(i<sums.size()) {
+                ans += sums[i];
+                ans = ans%MOD;
+            }
+        }
+
+        return ans;
+    }
+private:
+    const long MOD = pow(10, 9) + 7;    
+};
