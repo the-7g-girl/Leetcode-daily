8ce122eba53934fd9e15876d33658700c0de2454
Sharvani Jadhav <sharvaniuj21@iitk.ac.in>
2024-08-08 01:24:32 +0530

    Time: 6 ms (63.24%) | Memory: 16.9 MB (50.97%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -4,12 +4,12 @@ public:
         int m=image.size();
         int n=image[0].size();
         int ini=image[sr][sc];
-        dfs(image,sr,sc,m,n,color,ini);
+        if(ini!=color) dfs(image,sr,sc,m,n,color,ini);
         return image;
     }
     void dfs(vector<vector<int>>& image, int i, int j, int m, int n,int color, int ini){
         if(i< 0 || j<0 || i>=m || j>=n) return;
-        if(image[i][j]!=ini || image[i][j]==color) return;
+        if(image[i][j]!=ini ) return;
         image[i][j]=color;
         dfs(image,i+1,j,m,n,color,ini);
         dfs(image,i-1,j,m,n,color,ini);
