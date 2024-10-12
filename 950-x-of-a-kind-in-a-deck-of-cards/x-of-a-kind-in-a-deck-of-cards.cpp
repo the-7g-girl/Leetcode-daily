19528a338406d926e9e6352e3b145a6b163910ab
Sharvani Jadhav <sharvaniuj21@iitk.ac.in>
2024-10-12 16:14:25 +0530

    Time: 13 ms (44.97%) | Memory: 21.4 MB (17.30%) - LeetSync

--- 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.cpp
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    int gcdfun(int a, int b){
+        if(b>a) swap(a,b);
+        while(b!=0){
+            int t=a%b;
+            a=b;
+            b=t;
+        }
+        return a;
+    }
     bool hasGroupsSizeX(vector<int>& deck) {
         unordered_map<int,int> mp;
         for(int k: deck) mp[k]++;
@@ -7,7 +16,7 @@ public:
         if(deck.size()==1) return false;
         int t=mp[deck[0]];
         for(auto m :mp){
-            t=gcd(t,m.second);
+            t=gcdfun(t,m.second);
         }
         return t>1;
     }
