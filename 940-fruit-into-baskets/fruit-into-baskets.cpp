2f94ad2e788209f9cb81ef67c78569ea7a15d803
Sharvani Jadhav <sharvaniuj21@iitk.ac.in>
2024-10-13 00:40:46 +0530

    Time: 109 ms (86.45%) | Memory: 75.1 MB (68.06%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& nums) {
+        int l=0, r=0;
+        int cnt=0, ans=0;
+        unordered_map<int,int> s;
+        while( r<nums.size()){
+            while(s.size()<=2 && r<nums.size()){
+                s[nums[r]]++;
+                r++;
+            }
+            if(s.size()>2){
+                s[nums[l]]--;
+                if (s[nums[l]] == 0) {
+                    s.erase(nums[l]);
+                }
+                l++;
+            }
+            ans=max(ans, r-l);
+        }
+        return ans;
+    }
+};
