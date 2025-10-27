9ed97cb0e0faf39a0d41de2ca4f60520ddb710aa
Sharvani Jadhav <sharvani.jadhav13@gmail.com>
2025-10-27 16:42:30 +0530

    Time: 7 ms (6.00%) | Memory: 17.8 MB (49.17%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -3,13 +3,11 @@ class Solution:
         ans = ""
         i = 0
         while i < len(s):
-            print("i", i)
             if i < len(s) - 1 and s[i] != s[i+1] and s[i].lower() == s[i+1].lower():
                 i += 2
             else:
                 ans = ans + s[i]
                 i += 1
-        
         if ans == s:
             return ans
         return self.makeGood(ans)
